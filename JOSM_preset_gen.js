17db3605d89226685860c2d7d4a843cc82d620d2
Mariko Hisakawa <avenafatua@gmail.com>
2018-02-17 23:42:29 +0900

    Create common module (to support more templates)

---
+++ JOSM_preset_gen.js
@@ -0,0 +1,195 @@
+/**
+	JOSM_preset_gen.js
+	Yet Another Preset Generator for JOSM
+	for Node.js v6 or later
+	
+	Author: Maripo GODA
+*/
+
+let fs = require('fs');
+let builder = require('xmlbuilder');
+
+
+function read (file, onSuccess, onError) {
+    fs.readFile(file, 'utf8', function(err, data) {
+        if (err) {
+            onError(err);
+        } else {
+        		onSuccess(data);
+        }
+    });
+}
+
+class Group {
+	constructor (values) {
+		this.name = values[0];
+	}
+	appendNodeTo (parentNode) {
+		return parentNode.ele("group",{name:this.name});
+	}
+}
+
+class Preset {
+	constructor (values) {
+		this.values = values;
+	}
+	appendNodeTo (parentNode, conf) {
+		let columns = conf.columns;
+		let presetName = this.values[0];
+		let itemNode = parentNode.ele("item", {"name":presetName});
+		
+		if (conf.presetHeader) {
+			conf.presetHeader(itemNode, this.values);
+		}
+		
+		for (let i=0; i<Math.min(this.values.length, columns.length); i++) {
+			columns[i].append(itemNode, this.values[i]);
+		}
+		if (conf.presetFooter) {
+			conf.presetFooter(itemNode, this.values);
+		}
+		return itemNode;
+	}
+}
+
+const PATH_TYPE_ERROR = 0;
+const PATH_TYPE_ITEM = 1;
+const PATH_TYPE_GROUP = 2;
+
+function parseHierarchy (str) {
+	let depth = 0;
+	str = "" + str;
+	for (let i=0; i<str.length; i++) {
+		let ch = (str).charAt(i);
+		if (ch=='-') {
+			return {type:PATH_TYPE_ITEM, depth:depth};
+		}
+		if (ch=='+') {
+			return {type:PATH_TYPE_GROUP, depth:depth};
+		}
+		depth++;
+	}
+	return {type:PATH_TYPE_ERROR}
+}
+
+function generateXML (content, conf) {
+	let lines = content.split("\n");
+	
+	let xml = builder.create("presets");
+	xml.att("xmlns","http://josm.openstreetmap.de/tagging-preset-1.0");
+	if (conf.preset_attrs) {
+		for (let key in conf.preset_attrs) {
+			xml.att(key, conf.preset_attrs[key]);
+		}
+	}
+	if (conf.add_chunks) {
+		conf.add_chunks(xml);
+	}
+	
+	
+	// Container of all trees
+	let group;
+	if (conf.name) {
+		group = xml.ele("group", {"name":conf.name});
+	} else {
+		group = xml;
+	}
+		 	
+	let breadcrumb = [group];
+	let depth = 0;
+	for (let i=1; i<lines.length; i++) {
+		let values = lines[i].replace("\r","").replace("\n","").split("\t");
+		let pathStr = values.splice(0, 1);
+		let path = parseHierarchy(pathStr);
+		if (path.type==PATH_TYPE_ITEM) {
+			breadcrumb[path.depth+1] = new Preset(values).appendNodeTo(breadcrumb[path.depth], conf);
+		}
+		 else {
+		 	breadcrumb[path.depth+1] = new Group(values).appendNodeTo(breadcrumb[path.depth]);
+		 }
+	}
+	return xml;
+}
+exports.generatePresets = function (conf) {
+	let inFile = null;
+	let outFile = null;
+	process.argv.forEach(function (value, index, array) {
+		if (index == 2)
+			inFile = value;
+		if (index == 3)
+			outFile = value;
+	});
+	if (!inFile || !outFile) {
+		console.log("Usage:  node JOSM_tree_preset_gen.js <in_file> <out_file>");
+		return;
+	}
+	read(inFile, function (content) {
+			let xml = generateXML(content, conf);
+			fs.writeFileSync(outFile, xml.end({pretty:true}));
+		}, 
+		function(err){
+			console.log("Error " + err);
+		}
+	);
+}
+
+exports.FieldRootAttr = class FieldRootAttr {
+	constructor (key) {
+		this.key = key;
+	}
+	append (node, value) {
+		if (value && value.length) {
+			node.att(this.key, value);
+		}
+	}
+};
+
+
+// Simple Key-value pair
+exports.FieldText = class FieldText {
+	constructor (key) {
+		this.key = key;
+	}
+	append (node, value) {
+		if (value && value.length > 0) {
+			node.ele("key", {"key":this.key, "value":value});
+		}
+	}
+}
+
+// Key-value field (defined by dropdown options)
+exports.FieldAnnotatedText = class FieldAnnotatedText {
+	constructor (key) {
+		this.key = key;
+	}
+	append (node, value) {
+		value = value.replace(/ \(.*\)/, "");
+		if (value && value.length > 0) {
+			node.ele("key", {"key":this.key, "value":value});
+		}
+	}
+}
+
+// Dropdown menu
+exports.FieldCombo = class FieldCombo {
+	constructor (key, label, values) {
+		this.key = key;
+		this.label = label;
+		this.values = values;
+	}
+	append (node, value) {
+		if (value == "yes") {
+			node.ele("combo", {"key":this.key, "text":this.label, "values":this.values.join(",")});
+		}
+	}
+}
+
+exports.FieldLink = class FieldLink {
+	constructor () {
+	}
+	append (node, value) {
+		if (value && value.length > 0) {
+			node.ele("link", {"href":"https://www.wikidata.org/wiki/" + value});
+		}
+	}
+}
--- JOSM_tree_preset_gen.js
+++ JOSM_tree_preset_gen.js
@@ -1,207 +1,65 @@
 /**
+	JOSM_tree_preset_gen.js
 	Yet Another Tree Preset Generator for JOSM
-	Node.js
+	for Node.js v6 or later
+	
+	Author: Maripo GODA
 */
 
-let fs = require('fs');
-let builder = require('xmlbuilder');
-
-function read (file, onSuccess, onError) {
-    fs.readFile(file, 'utf8', function(err, data) {
-        if (err) {
-            onError(err);
-        } else {
-        		onSuccess(data);
-        }
-    });
-}
-
-// Attribute of item's node itself
-class FieldRootAttr {
-	constructor (key) {
-		this.key = key;
-	}
-	append (node, value) {
-		if (value && value.length) {
-			node.att(this.key, value);
-		}
-	}
-}
-
-// Simple Key-value pair
-class FieldText {
-	constructor (key) {
-		this.key = key;
-	}
-	append (node, value) {
-		if (value && value.length > 0) {
-			node.ele("key", {"key":this.key, "value":value});
-		}
-	}
-}
-
-// Key-value field (defined by dropdown options)
-class FieldAnnotatedText {
-	constructor (key) {
-		this.key = key;
-	}
-	append (node, value) {
-		value = value.replace(/ \(.*\)/, "");
-		if (value && value.length > 0) {
-			node.ele("key", {"key":this.key, "value":value});
-		}
-	}
-}
-
-// Dropdown menu
-class FieldCombo {
-	constructor (key, label, values) {
-		this.key = key;
-		this.label = label;
-		this.values = values;
-	}
-	append (node, value) {
-		if (value == "yes") {
-			node.ele("combo", {"key":this.key, "text":this.label, "values":this.values.join(",")});
-		}
-	}
-}
-
-class FieldLink {
-	constructor () {
-	}
-	append (node, value) {
-		if (value && value.length > 0) {
-			node.ele("link", {"href":"https://www.wikidata.org/wiki/" + value});
-		}
-	}
-}
+const pg = require("./JOSM_preset_gen.js");
 
 const COLUMNS = [
-	new FieldRootAttr("name"),
-	new FieldAnnotatedText("leaf_type"),
-	new FieldText("genus"),
-	new FieldText("species"),
-	new FieldText("genus:ja"),
-	new FieldText("genus:en"),
-	new FieldText("taxon"),
-	new FieldCombo("sex", "雌雄", ["male","female"]),
-	new FieldAnnotatedText("leaf_cycle"),
-	new FieldLink()
+	new pg.FieldRootAttr("name"),
+	new pg.FieldAnnotatedText("leaf_type"),
+	new pg.FieldText("genus"),
+	new pg.FieldText("species"),
+	new pg.FieldText("genus:ja"),
+	new pg.FieldText("genus:en"),
+	new pg.FieldText("taxon"),
+	new pg.FieldCombo("sex", "雌雄", ["male","female"]),
+	new pg.FieldAnnotatedText("leaf_cycle"),
+	new pg.FieldLink()
 ];
 
-class Group {
-	constructor (values) {
-		this.name = values[0];
+function isNeedleleaved (values) {
+	for (let i=0; i<values.length; i++) {
+		if (values[i].indexOf("needleleaved")>=0) {
+			return true;
+		}
 	}
-	appendNodeTo (parentNode) {
-		return parentNode.ele("group",{name:this.name});
-	}
+	return false;
 }
-
-class Preset {
-	constructor (values) {
-		this.values = values;
-	}
-	isNeedleleaved () {
-		for (let i=0; i<this.values.length; i++) {
-			if (this.values[i].indexOf("needleleaved")>=0) {
-				return true;
-			}
-		}
-		return false;
-	}
-	appendNodeTo (parentNode) {
-		let presetName = this.values[0];
-		let itemNode = parentNode.ele("item", {"name":presetName, 
-			"icon":this.isNeedleleaved()?"presets/landmark/trees_conifer.svg":"presets/landmark/trees.svg"});
-		
-		// default fields (label, name)
-		itemNode.ele("label", {"text":presetName});
-		itemNode.ele("text",{ "key":"name", "text":"名前" });
-		
-		for (let i=0; i<Math.min(this.values.length, COLUMNS.length); i++) {
-			COLUMNS[i].append(itemNode, this.values[i]);
-		}
-		itemNode.ele("reference", {"ref":"japan_trees"});
-		return itemNode;
-	}
-}
-
-const PATH_TYPE_ERROR = 0;
-const PATH_TYPE_ITEM = 1;
-const PATH_TYPE_GROUP = 2;
-
-function parseHierarchy (str) {
-	let depth = 0;
-	str = "" + str;
-	for (let i=0; i<str.length; i++) {
-		let ch = (str).charAt(i);
-		if (ch=='-') {
-			return {type:PATH_TYPE_ITEM, depth:depth};
-		}
-		if (ch=='+') {
-			return {type:PATH_TYPE_GROUP, depth:depth};
-		}
-		depth++;
-	}
-	return {type:PATH_TYPE_ERROR}
-}
-
-function parse (content) {
-	let lines = content.split("\n");
-	
-	let xml = builder.create("presets");
-	xml.att("xmlns","http://josm.openstreetmap.de/tagging-preset-1.0");
-	xml.att("author","Maripo GODA and contributors");
-	xml.att("description","日本でよく見られる樹木のプリセットです");
-	xml.att("shortdescription","日本の樹木");
-	
-	// Default fields
-	let chunk = xml.ele("chunk",{"id":"japan_trees"});
-	chunk.ele("text",{ "key":"operator", "text":"運営者" });
-	chunk.ele("text",{ "key":"ref", "text":"参照番号" });
-	chunk.ele("text",{ "key":"height", "text":"高さ(m)" });
-	chunk.ele("text",{ "key":"circumference", "text":"幹の外周(m)" });
-	chunk.ele("combo", {"key":"denotation", "text":"特徴", "values":"landmark,natural_monument,avenue,urban"});
-	chunk.ele("check", {"key":"monument", "text":"記念樹"});
-	
-	// Container of all trees
-	let group = xml.ele("group", {"name":"樹木"});
-		 	
-	let breadcrumb = [group];
-	let depth = 0;
-	for (let i=1; i<lines.length; i++) {
-		let values = lines[i].replace("\r","").replace("\n","").split("\t");
-		let pathStr = values.splice(0, 1);
-		let path = parseHierarchy(pathStr);
-		if (path.type==PATH_TYPE_ITEM) {
-			breadcrumb[path.depth+1] = new Preset(values).appendNodeTo(breadcrumb[path.depth]);
-		}
-		 else {
-		 	breadcrumb[path.depth+1] = new Group(values).appendNodeTo(breadcrumb[path.depth]);
-		 }
-	}
-	return xml;
-}
-
 (function(){
-	let inFile = null;
-	let outFile = null;
-	process.argv.forEach(function (value, index, array) {
-		if (index == 2)
-			inFile = value;
-		if (index == 3)
-			outFile = value;
-	});
-	if (!inFile || !outFile) {
-		console.log("Usage:  node JOSM_tree_preset_gen.js <in_file> <out_file>");
-		return;
-	}
-	read(inFile, function (content) {
-			let xml = parse(content);
-			fs.writeFileSync(outFile, xml.end({pretty:true}));
-		}, 
-		function(err){
-			console.log("Error " + err);});
+	
+	let conf = {
+		name: "樹木",
+		preset_attrs : {
+			author : "Maripo GODA and contributors",
+			description : "日本でよく見られる樹木のプリセットです",
+			shortdescription : "日本の樹木"
+		},
+		columns: COLUMNS,
+		add_chunks : function (xml) {
+			// Default fields
+			let chunk = xml.ele("chunk",{"id":"japan_trees"});
+			chunk.ele("text",{ "key":"operator", "text":"運営者" });
+			chunk.ele("text",{ "key":"ref", "text":"参照番号" });
+			chunk.ele("text",{ "key":"height", "text":"高さ(m)" });
+			chunk.ele("text",{ "key":"circumference", "text":"幹の外周(m)" });
+			chunk.ele("combo", {"key":"denotation", "text":"特徴", "values":"landmark,natural_monument,avenue,urban"});
+			chunk.ele("check", {"key":"monument", "text":"記念樹"});
+		},
+		presetHeader: function (itemNode, values) {
+			
+			itemNode.att("icon", isNeedleleaved(values)?"presets/landmark/trees_conifer.svg":"presets/landmark/trees.svg");
+			itemNode.ele("label", {"text":values[0]});
+			itemNode.ele("text",{ "key":"name", "text":"名前" });
+		
+		},
+		presetFooter: function (itemNode, values) {
+			itemNode.ele("reference", {"ref":"japan_trees"});
+		
+		}
+	};
+	pg.generatePresets(conf);
 })();
